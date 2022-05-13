b27332e42826019d54ff3fa0c4657b51252b7f9a
Domingos Masta <e1000014692@gmail.com>
2022-05-13 13:19:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rap Batle',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
